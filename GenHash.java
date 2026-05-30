a63b2968944473b280606cbebf29c103c0497c22
Juan Sebastian Rincon Farelo <juansebastianrincon@unicesar.edu.co>
2026-05-29 22:11:18 -0500

    chore: add GenHash.java and wallet_part*.txt to .gitignore, remove from tracking

--- GenHash.java
+++
@@ -1,6 +0,0 @@
-import org.mindrot.jbcrypt.BCrypt;
-public class GenHash {
-    public static void main(String[] args) {
-        System.out.println(BCrypt.hashpw("admin123", BCrypt.gensalt(12)));
-    }
-}
